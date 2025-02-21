df02f6ce6bbd508b9646071a1183723ed1f5f658
Sebastian Dolfen <sebi.dolfen.sd@gmail.com>
2025-02-22 00:28:44 +0100

    Refactor Sidebar menu item routing for localization and update locale files

--- components/Navigation/Sidebar/Menu/NavigationSidebarMenuItemStatic.vue
+++ components/Navigation/Sidebar/Menu/NavigationSidebarMenuItemStatic.vue
@@ -1,30 +1,31 @@
 <template>
-  <SidebarMenuItem>
-    <SidebarMenuButton
-      :tooltip="item.title"
-      :is-active="
-        (route.path.includes(item.urlCompare) && item.urlCompare != '/') ||
-        (item.urlCompare == '/' && route.path == '/')
-      "
-      as-child
-    >
-      <i18n-link :to="item.location" class="w-full !text-md flex flex-row items-center py-5 pr-3 pl-[calc(0.75rem-2px)] rounded-none">
-        <component :is="item.icon" />
-        <span>{{ $t(item.title) }}</span>
-      </i18n-link>
-    </SidebarMenuButton>
-  </SidebarMenuItem>
+	<SidebarMenuItem>
+		<SidebarMenuButton
+			:tooltip="item.title"
+			:is-active="route.name?.toString().startsWith('localized-'+item.urlCompare)"
+			as-child
+		>
+			<i18n-link
+				:to="item.location"
+				class="w-full !text-md flex flex-row items-center py-5 pr-3 pl-[calc(0.75rem-2px)] rounded-none"
+			>
+				<component :is="item.icon" />
+				<span>{{ $t(item.title) }}</span>
+			</i18n-link>
+		</SidebarMenuButton>
+	</SidebarMenuItem>
 </template>
 
 <script lang="ts" setup>
 defineProps({
-  item: {
-    type: Object as PropType<SidebarMenuItem>,
-    required: true,
-  },
+	item: {
+		type: Object as PropType<SidebarMenuItem>,
+		required: true,
+	},
 });
 
 var route = useRoute();
+console.log(route.name)
 </script>
 
 <style lang="scss" scoped></style>
--- utils/navigationMenu.ts
+++ utils/navigationMenu.ts
@@ -12,31 +12,31 @@ export default () => {
 			title: "layout-default.menu.home",
 			icon: LucideHome,
 			location: { name: "index" },
-			urlCompare: "/",
+			urlCompare: "index",
 		},
 		{
 			title: "layout-default.menu.carrier",
 			icon: LucideOctagonAlert,
 			location: { name: "carrier" },
-			urlCompare: "/carrier",
+			urlCompare: "carrier",
 		},
 		{
 			title: "layout-default.menu.information",
 			icon: LucideFileArchive,
 			location: { name: "information" },
-			urlCompare: "/information",
+			urlCompare: "information",
 		},
 		{
 			title: "layout-default.menu.commanders",
 			icon: LucideUsers,
 			location: { name: "commanders" },
-			urlCompare: "/commanders",
+			urlCompare: "commanders",
 		},
 		{
 			title: "layout-default.menu.gallery",
 			icon: LucideImages,
 			location: { name: "gallery" },
-			urlCompare: "/gallery",
+			urlCompare: "gallery",
 		},
 	];
 	return items;
